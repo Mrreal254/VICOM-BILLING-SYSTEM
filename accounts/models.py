eb62aac638f9e63128c711c51a6dc7859a8198ad
Mrreal254 <vicomcommenterprise@gmail.com>
2026-09-21 23:15:53 +0300

    fix: make VICOM authentication email based

--- accounts/models.py
+++ accounts/models.py
@@ -3,6 +3,8 @@ import uuid
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from .managers import UserManager
+
 
 class User(AbstractUser):
     class Role(models.TextChoices):
@@ -11,6 +13,7 @@ class User(AbstractUser):
         ISP_STAFF = "ISP_STAFF", "ISP Staff"
         CUSTOMER = "CUSTOMER", "Customer"
 
+    username = None
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(unique=True)
     role = models.CharField(max_length=20, choices=Role.choices, default=Role.ISP_STAFF)
@@ -24,6 +27,10 @@ class User(AbstractUser):
     )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+    objects = UserManager()
 
     def __str__(self):
         return self.email
